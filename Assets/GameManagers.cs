40f8d5d02d7c02e5240504f5a71898f1c9c9c33d
logancho <logancho04@gmail.com>
2023-05-04 18:11:46 -0400

    Working raycasted button UI, can use for start/end screens.

--- Assets/GameManagers.cs
+++ Assets/GameManagers.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Valve.VR.Extras;
 
 public class GameManagers : MonoBehaviour
 {
@@ -8,7 +11,55 @@ public class GameManagers : MonoBehaviour
     // Start is called before the first frame update
     public void StartGame()
     {
+        Debug.Log("WHAAAt");
         startGamePanel.SetActive(false);
     }
+    /*
 
+    public SteamVR_LaserPointer laserPointerLeft;
+    public SteamVR_LaserPointer laserPointerRight;
+
+    void Awake()
+    {
+        laserPointerLeft.PointerIn += PointerInside;
+        laserPointerLeft.PointerOut += PointerOutside;
+        laserPointerLeft.PointerClick += PointerClick;
+    }
+
+    public void PointerClick(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was clicked");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was clicked");
+        }
+    }
+
+    public void PointerInside(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was entered");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was entered");
+        }
+    }
+
+    public void PointerOutside(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was exited");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was exited");
+        }
+    }
+    */
 }
